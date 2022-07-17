ed00b5176654de92dd7fea166f9296e6b2460587
samer411 <samer.nassim.behery@gmail.com>
2022-07-18 01:28:45 +0200

    18/7/2022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './mainLayout/side-bar/side-bar.component';
+import { NavBarComponent } from './mainLayout/nav-bar/nav-bar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    NavBarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule

5731362f56caec3b37113621ec01f6baab609ee1
samer411 <samer.nassim.behery@gmail.com>
2022-07-20 18:23:50 +0200

    18/7/2022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,26 @@ import { AppComponent } from './app.component';
 import { SideBarComponent } from './mainLayout/side-bar/side-bar.component';
 import { NavBarComponent } from './mainLayout/nav-bar/nav-bar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule } from 'primeng/calendar';
+import {AccordionModule} from 'primeng/accordion';     
+import {MenuItem} from 'primeng/api';  
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
     NavBarComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CalendarModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/mainLayout/nav-bar/nav-bar.component.ts
+++ src/app/mainLayout/nav-bar/nav-bar.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { CalendarModule } from 'primeng/calendar';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class NavBarComponent implements OnInit {
-
-  constructor() { }
+  date: Date = new Date();
+  constructor(private primengConfig: PrimeNGConfig) {}
 
   ngOnInit(): void {
+    this.primengConfig.ripple = true;
   }
+  onButtonGroupClick($event: { target: any; srcElement: any }) {
+    let clickedElement = $event.target || $event.srcElement;
 
+    if (clickedElement.nodeName === 'BUTTON') {
+      let isCertainButtonAlreadyActive =
+        clickedElement.parentElement.querySelector('.active');
+      // if a Button already has Class: .active
+      if (isCertainButtonAlreadyActive) {
+        isCertainButtonAlreadyActive.classList.remove('active');
+      }
+
+      clickedElement.className += ' active';
+    }
+  }
 }
